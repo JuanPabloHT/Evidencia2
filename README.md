d44751f982f26c714e9d9c735c19a2e30d7e73bd
JuanPabloHT <137842835+JuanPabloHT@users.noreply.github.com>
2024-11-28 20:44:32 +0000

    terminados

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Evidencia2
+## Evidencia2
+# Soluciones de la Evidencia 2: Programación de Estructuras de Datos y Algoritmos Fundamentales
+
+Este repositorio contiene las soluciones de los problemas correspondientes a la evidencia dos de la materia *Programación de Estructuras de Datos y Algoritmos Fundamentales*.
+
+
+
+
+
---
+++ problema1/ClosestNumbers.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'closestNumbers' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; ++j) {
+        if (arr[j] < pivot) {
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+vector<int> closestNumbers(vector<int> arr) {
+    quickSort(arr, 0, arr.size() - 1);
+
+    int min_diff = INT_MAX;
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        int diff = arr[i + 1] - arr[i];
+        if (diff < min_diff) {
+            min_diff = diff;
+        }
+    }
+
+    vector<int> result;
+    for (size_t i = 0; i < arr.size() - 1; ++i) {
+        if (arr[i + 1] - arr[i] == min_diff) {
+            result.push_back(arr[i]);
+            result.push_back(arr[i + 1]);
+        }
+    }
+
+    return result;
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = closestNumbers(arr);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
